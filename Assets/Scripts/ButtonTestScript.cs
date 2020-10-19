132403e9376e8661471df3074869184b72a25861
JejySan <37908694+JejySan@users.noreply.github.com>
2020-10-19 04:22:17 +0200

    Some tests are done here
    Still a noob at Unity dev, but i made things work like i want actually. Next time, the real game content ! (i hope)

---
+++ Assets/Scripts/ButtonTestScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTestScript : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    public void CayLeTest()
+    {
+        Debug.Log("Ça marche là ?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ChangeClothScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeClothScript : MonoBehaviour
+{
+    public GameObject SelectedCloth, spawnPoint, hatSpawnPoint, torsoSpawnPoint, legSpawnPoint, footSpawnPoint, instance;
+    public float ScaleReduce;
+    public bool clothPut, clothPutHat, clothPutTorso, clothPutLeg, clothPutFoot;
+    public string clothName, clothNameHat, clothNameTorso, clothNameLeg, clothNameFoot;
+
+    public void PutCloth()
+    {
+        clothName = SelectedCloth.name;
+        if(spawnPoint.transform.Find(clothName+"(Clone)") != null) // Pour vérifier si y'a pas déjà quelque chose là ou on veut faire spawner notre vêtement
+        {
+            Destroy(instance);
+            Debug.Log(SelectedCloth+" viré");
+        }
+        instance = Instantiate(SelectedCloth, spawnPoint.transform);
+        instance.transform.localScale = instance.transform.localScale / ScaleReduce; // La variable ScaleReduce j'ai dû la mettre parce que, pour une raison encore inconnue, l'armature importée est de taille x100
+        
+        
+        Debug.Log("Et Hop !");
+        return;
+    }
+
+    public void HatPut()
+    {
+
+    }
+    public void TorsoPut()
+    {
+        
+    }
+    public void LegPut()
+    {
+        
+    }
+    public void FootPut()
+    {
+        
+    }
+    public void HatDisabled()
+    {
+        
+    }
+    public void TorsoDisabled()
+    {
+        
+    }
+    public void LegDisabled()
+    {
+        
+    }
+    public void FootDisabled()
+    {
+        
+    }
+}
